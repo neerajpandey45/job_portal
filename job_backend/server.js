6716fecbd5c7b89118f74648e3b18a562c0d74a1
NEERAJ PANDEY <neerajpandey4578@gmail.com>
2025-09-30 16:07:56 +0530

    create frontend deployment url

--- job_backend/server.js
+++ job_backend/server.js
@@ -24,8 +24,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
-  "http://localhost:3000",
-  "http://10.233.38.43:3000", // ✅ your laptop's IP (accessed by mobile)
+  "https://job-portal-theta-lemon.vercel.app"
+  //  origin: "https://job-portal-theta-lemon.vercel.app"// your frontend URL
+  // "http://localhost:3000",
+  // "http://10.233.38.43:3000", // ✅ your laptop's IP (accessed by mobile)
 ];
 // Middlewares
 app.use(cors({
